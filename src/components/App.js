8f4932302715a9aa46502eb53483178de8b89c4b
Kash-2g <162958950+Kash-2g@users.noreply.github.com>
2024-05-09 13:07:00 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
@@ -13,6 +13,8 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Home name={user.name} city={user.city} color={user.color} /> 
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} /> 
     </div>
   );
 }
